fef4191423ac70e25a802b453cb58919fec8e83e
UglyDima <dimaparkhomchuk2912@gmail.com>
2026-09-16 16:51:05 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+/* =========================
+   DUEL BUTTON
+========================= */
 
+document
+    .getElementById("duelButton")
+    .addEventListener(
+        "click",
+        function () {
+
+            this.textContent =
+                "Поиск соперника...";
+
+            setTimeout(() => {
+
+                this.textContent =
+                    "Играть →";
+
+                alert(
+                    "DUEL пока находится в разработке."
+                );
+
+            }, 700);
+        }
+    );
